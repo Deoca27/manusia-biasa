f3a21bcfb6dad57b37fe9fab1c8502ffcc5b91a3
maechel11 <jhoshemaechel01@gmail.com>
2025-01-05 15:56:54 +0700

    jhoshe: membuat tampilan page admin (blm selesai)

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,13 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function RootPage() {
+export default function AdminPage() {
   return (
     <>
+    aaaaaaaaaaa
+    
     </>
   );
 }
