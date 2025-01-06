33a8962e0544b21c22014c33951838551d2e974d
maechel11 <jhoshemaechel01@gmail.com>
2025-01-06 14:06:10 +0700

    jhoshe: membuat judul tabel untuk tabel admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="lemonade" >
+    <html lang="en" data-theme="lemonade">
       <body>
         {/* area header */}
         <header>
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,36 @@
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
-import Link from "next/link";
+"use client";
 
 export default function AdminPage() {
   return (
     <>
-    aaaaaaaaaaa
-    
+      <table className='w-full shadow-md '>
+        {/* judul tabel admin */}
+        <thead >
+          <tr className="border border-slate-300 ">
+            <th className='w-5% text-start p-2'>No</th>
+            <th className='w-1/2 text-start p-2'>Nama</th>
+            <th className='w-40% text-start p-2'>password</th>
+            <th className='w-5% text-center'>a</th>
+          </tr>          
+        </thead>
+        <tbody>
+          <tr className="border border-slate-300 ">
+            <td className="p-2">
+              a
+            </td>
+            <td className="p-2">
+              b
+            </td>
+            <td className="p-2">
+              c
+            </td>
+            <td className="p-2 ">
+              c
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
     </>
   );
 }
