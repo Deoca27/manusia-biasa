58d880e0cd07f9991ca004c6de5d6ec3b6996fb5
maechel11 <jhoshemaechel01@gmail.com>
2025-01-07 17:51:46 +0700

    jhoshe: memperbaiki kesalahan admin>layout.tsx agar tidak tumpang tindih dengan layout.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,7 +11,7 @@ export default function AdminPage() {
             <th className='w-1/2 text-start p-2'>Nama</th>
             <th className='w-40% text-start p-2'>password</th>
             <th className='w-5% text-center'>a</th>
-          </tr>          
+          </tr>
         </thead>
         <tbody>
           <tr className="border border-slate-300 ">
