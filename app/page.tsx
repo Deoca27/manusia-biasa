77eac984ce387470101f127a899e7ebf8bb8dcbf
Rendika Uhuyyy <rendikauhuyyy@gmail.com>
2025-01-08 21:45:17 +0700

    rendika : membuat countener baru untuk bacground judul, dan deskripsi

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 
 export default function RootPage() {
   return (
-    <>
-    </>
+    <main>
+      {/* Hero Section */}
+      <div className="relative bg-gray-900 text-white min-h-[600px] flex items-center">
+        <div className="container mx-auto px-6 py-12">
+          
+        </div>
+      </div>
+    </main>
   );
 }
