a72a4fe7233f85de00df702e0bc0eb83b3145bcd
Rendika Uhuyyy <rendikauhuyyy@gmail.com>
2025-01-08 22:01:25 +0700

    rendika : menambahkan teks judul

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function RootPage() {
       {/* Hero Section */}
       <div className="relative bg-gray-900 text-white min-h-[600px] flex items-center">
         <div className="container mx-auto px-6 py-12">
-          
+        <h1 className="text-6xl font-bold mb-6">Electronic Hub</h1>
         </div>
       </div>
     </main>
