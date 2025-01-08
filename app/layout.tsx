a8c4b9ebe9b101dc7beb1625d7fbe43f169a69e5
Deoca27 <pikirsendiri953@gmail.com>
2025-01-08 20:52:49 +0700

    deo : membuat tombol berada di kanan

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         {/* Area Header */}
         <header>
           <Link href={"/"}>Ecommerce</Link>
-          <nav>
+          <nav className="flex justify-end">
             <Link href={"/"}>Home</Link>
             <Link href={"/"}>About</Link>
             <Link href={"/"}>Products</Link>
