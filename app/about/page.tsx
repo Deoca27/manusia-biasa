7e035ff72de0c189b875229cec479235113ee4f8
rendikayp01 <rendikayogapamungkas@gmail.com>
2025-01-09 16:18:18 +0700

    rendika : menambahkan Judul about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,9 @@ export default function AboutPage() {
         <>
             {/* Hero Section */}
             <div className="relative bg-gray-900 text-white py-20">
+                <div className="container mx-auto px-6">
+                <h1 className="text-5xl font-bold mb-6 text-center">About Us</h1>
+                </div>
             </div>
         </>
     );
