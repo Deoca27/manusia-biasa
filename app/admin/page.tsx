9a7002a85162dbf99f5c7a2cf3cdb31607d4e376
maechel11 <jhoshemaechel01@gmail.com>
2025-01-10 11:05:45 +0700

    jhoshe: Merubah teks di dropdown

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -44,10 +44,10 @@ export default function AdminPage() {
               </td>
               <td className="text-right pr-4 px-2">
                 <div className="dropdown dropdown-bottom">
-                  <div tabIndex={0} role="button" className="btn m-1">Click</div>
+                  <div tabIndex={0} role="button" className="btn m-1">...</div>
                   <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
+                    <li><a>Hapus</a></li>
+                    <li><a>Edit</a></li>
                   </ul>
                 </div>
               </td>
