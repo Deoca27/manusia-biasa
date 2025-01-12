2bc04ed100ae036bd0de1d7da1a02e77ce6a65b6
Deoca27 <pikirsendiri953@gmail.com>
2025-01-13 01:44:48 +0700

    deo : mengatur map menggunakan tailwind

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,8 +5,7 @@ export default function ContactPage() {
     <div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.2266202168294!2d105.25524097438428!3d-5.382383994596563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e40dadc7970b277%3A0x5b1fe57f83b6416c!2sUniversitas%20Teknokrat%20Indonesia!5e0!3m2!1sid!2sid!4v1736705521498!5m2!1sid!2sid"
-        width="600"
-        height="450"
+        className="w-full h-80 border-0"
         allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
