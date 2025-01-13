ca5bb3dc1244f009bba9246750b6a1614402cdbf
maechel11 <jhoshemaechel01@gmail.com>
2025-01-13 21:26:06 +0700

    jhoshe: mengubah text dari button menjadi Add

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -37,7 +37,7 @@ export default function AdminPage() {
           onChange={(e) => {
           }} /></div>
 
-        <button className="btn col-end-11">
+        <button className="btn col-end-12">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
@@ -50,7 +50,7 @@ export default function AdminPage() {
               strokeWidth="2"
               d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
           </svg>
-          Button
+          Add
         </button>
       </div>
 
