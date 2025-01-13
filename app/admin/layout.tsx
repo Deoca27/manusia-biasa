7613eecea96bd81e8323519643e18f78506c2e04
maechel11 <jhoshemaechel01@gmail.com>
2025-01-14 00:52:47 +0700

    jhoshe: merubah header menjadi sidebar pada layout admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,55 +4,36 @@ import "../globals.css"
 //import font awesome
 import "@fortawesome/fontawesome-svg-core/styles.css"
 
-
-
-
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="layout-admin min-h-screen">
+    <div className="layout-admin min-h-screen flex">
 
-      {/* header */}
-      <header>
-        <div className="navbar bg-base-100">
-          <div className="flex-1">
-            <Link href={"/admin"} className="btn btn-ghost text-xl">Admin Controller</Link>
-            <Link href={"/admin/barang"} className="btn btn-ghost text-xl">Item Controller</Link>
-          </div>
-          <div className="flex-none gap-2">
-            
-            <div className="dropdown dropdown-end">
-              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img
-                    alt="Tailwind CSS Navbar component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li><a>Settings</a></li>
-                <li><a>Logout</a></li>
-              </ul>
-            </div>
-          </div>
+      {/* Sidebar */}
+      <aside className="w-60 bg-base-100 p-4 shadow-md flex flex-col justify-between">
+        <ul className="menu">
+          <li>
+            <Link href="/admin" className="btn btn-ghost">Admin Controller</Link>
+          </li>
+          <li>
+            <Link href="/admin/barang" className="btn btn-ghost">Item Controller</Link>
+          </li>
+        </ul>
+        <div className="mt-auto mb-8">
+          <Link href="/" className="btn btn-outline btn-error w-full">
+            Log Out
+          </Link>
         </div>
-      </header>
+      </aside>
 
-      {/* ini content */}
-      <section className="mx-8 my-4">{children}</section>
+      {/* Content */}
+      <main className="flex-1 mx-8 my-4">
+        {children}
+      </main>
 
     </div>
-
   )
 }
