5bc69dbd544df9fd0c43d5a28eb9f385da6551a1
maechel11 <jhoshemaechel01@gmail.com>
2025-01-13 21:33:37 +0700

    jhoshe: mengganti acon button/tombol add menjadi icon plus

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,7 @@ import { getData, setUpdateStatus } from "../models/admin";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 //buat fungsi untuk dialog hapus
 async function setDelete(username: string) {
@@ -38,18 +38,7 @@ export default function AdminPage() {
           }} /></div>
 
         <button className="btn col-end-12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-          </svg>
+        <FontAwesomeIcon icon={faPlus} />
           Add
         </button>
       </div>
