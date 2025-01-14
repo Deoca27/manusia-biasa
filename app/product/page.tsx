333005063cb1aba23955ce1615e537071629e921
Deoca27 <pikirsendiri953@gmail.com>
2025-01-14 23:58:59 +0700

    deo: menambah komentar

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from "react";
 import { getData } from "../models/barang";
 
 export default function ProductPage() {
+  //buat hook dengan "use  state"
   const [getValue, setValue] = useState<any[]>([]);
 
+  // buat fungsi untuk panggil "getData"
   async function fetchData() {
     const data = await getData();
     setValue(data);
   }
 
+  // hook dengan "use effect"
   useEffect(() => {
+    // panggil fungsi "getData"
     fetchData();
   }, []);
   
