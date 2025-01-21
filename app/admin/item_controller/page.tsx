b006b9a70ae9bb5e75cf2f18a0b858a32c16d7dd
maechel11 <jhoshemaechel01@gmail.com>
2025-01-21 18:02:52 +0700

    jhoshe: mengganti teks pada teks input menjadi search

--- app/admin/item_controller/page.tsx
+++ app/admin/item_controller/page.tsx
@@ -4,7 +4,7 @@ export default function BarangPage() {
   return (
     <>
     <div className="text-center pb-4 font-extrabold text-2xl">TABEL BARANG</div>
-    <div className=""><input type="text" placeholder="Type Here" className="input input-bordered w-full "
+    <div className=""><input type="text" placeholder="Search" className="input input-bordered w-full "
           onChange={(e) => {
           }} /></div>
     </>
