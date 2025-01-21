5bf95f3d1aa9b5f5c2e179011e9edc4ba84fae0a
maechel11 <jhoshemaechel01@gmail.com>
2025-01-21 18:08:56 +0700

    jhoshe: menambahkan col start dan end pada classname textinput search

--- app/admin/item_controller/page.tsx
+++ app/admin/item_controller/page.tsx
@@ -5,7 +5,7 @@ export default function BarangPage() {
     <>
     <div className="text-center pb-4 font-extrabold text-2xl">TABEL BARANG</div>
     <div className="grid grid-cols-10 gap-4">
-    <div className=""><input type="text" placeholder="Search" className="input input-bordered w-full "
+    <div className=" col-start-1 col-end-4"><input type="text" placeholder="Search" className="input input-bordered w-full "
           onChange={(e) => {
           }} />
           </div>
