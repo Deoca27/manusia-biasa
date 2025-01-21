3cb18fdcdf8533efb765af232b4c7a1893491752
maechel11 <jhoshemaechel01@gmail.com>
2025-01-21 15:22:50 +0700

    jhoshe: menambahkan tailwind padding pada TABEL BARANG

--- app/admin/item_controller/page.tsx
+++ app/admin/item_controller/page.tsx
@@ -3,7 +3,7 @@
 export default function BarangPage() {
   return (
     <>
-    <div className="text-center">TABEL BARANG</div>
+    <div className="text-center pb-4">TABEL BARANG</div>
     </>
   );
 }
