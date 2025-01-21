29f5aeb5d8acc9c3c25135400da24adfef7000e3
maechel11 <jhoshemaechel01@gmail.com>
2025-01-21 20:01:47 +0700

    jhoshe: Menambahkan tailwind Padding pada Header Table "Kategori"

--- app/admin/item_controller/page.tsx
+++ app/admin/item_controller/page.tsx
@@ -26,7 +26,7 @@ export default function BarangPage() {
             <th className="w-15% text-start p-2">Gambar</th>
             <th className="w-15% text-start p-2">Nama Barang</th>
             <th className="w-25% text-start p-2">Deskripsi</th>
-            <th className="w-10% text-start">Kategori</th>
+            <th className="w-10% text-start p-2">Kategori</th>
             <th className="w-15% text-start">Harga</th>
             <th className="w-10% text-start">Link Produk</th>
             <th className="w-5% text-center">Aksi</th>
