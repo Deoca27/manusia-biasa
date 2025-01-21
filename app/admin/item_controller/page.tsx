f2a0b70df9f1f4c591a7acc7646d49d35f016bfd
maechel11 <jhoshemaechel01@gmail.com>
2025-01-21 18:20:32 +0700

    jhoshe: menambahkan button dengan daisyUI

--- app/admin/item_controller/page.tsx
+++ app/admin/item_controller/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 
+import Link from "next/link";
+
 export default function BarangPage() {
   return (
     <>
-    <div className="text-center pb-4 font-extrabold text-2xl">TABEL BARANG</div>
-    <div className="grid grid-cols-10 gap-4">
-    <div className=" col-start-1 col-end-4"><input type="text" placeholder="Search" className="input input-bordered w-full "
+      <div className="text-center pb-4 font-extrabold text-2xl">TABEL BARANG</div>
+      <div className="grid grid-cols-10 gap-4">
+        <div className=" col-start-1 col-end-4"><input type="text" placeholder="Search" className="input input-bordered w-full "
           onChange={(e) => {
           }} />
-          </div>
-    </div>
+        </div>
+        <Link href={"/"} className="btn">
+          
+          Button
+        </Link>
+      </div>
     </>
   );
 }
