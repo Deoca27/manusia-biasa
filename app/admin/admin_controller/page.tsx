073fda84f7639e92ddbfb1aa8fbf3b87c91ad57c
maechel11 <jhoshemaechel01@gmail.com>
2025-01-21 18:07:30 +0700

    jhoshe: menambahkan grid pada text input

--- app/admin/admin_controller/page.tsx
+++ app/admin/admin_controller/page.tsx
@@ -33,7 +33,7 @@ export default function AdminPage() {
   return (
     <>
       <div className="text-center pb-4 font-extrabold text-2xl">TABEL ADMIN</div>
-      <div className="grid grid-cols-10 gap-4 items-center pb-2">
+      <div className="grid grid-cols-10 gap-4 pb-2">
         <div className=" col-start-1 col-end-4"><input type="text" placeholder="Search" className="input input-bordered w-full "
           onChange={(e) => {
           }} /></div>
--- app/admin/item_controller/page.tsx
+++ app/admin/item_controller/page.tsx
@@ -4,9 +4,12 @@ export default function BarangPage() {
   return (
     <>
     <div className="text-center pb-4 font-extrabold text-2xl">TABEL BARANG</div>
+    <div className="grid grid-cols-10 gap-4">
     <div className=""><input type="text" placeholder="Search" className="input input-bordered w-full "
           onChange={(e) => {
-          }} /></div>
+          }} />
+          </div>
+    </div>
     </>
   );
 }
