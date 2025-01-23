56878f806da01311bf582596110ce5ef0ccc1930
Deoca27 <pikirsendiri953@gmail.com>
2025-01-24 00:28:34 +0700

    deo : menambah tampilan gambar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { getData} from "./models/barang";
+import { getData } from "./models/barang";
 
 export default function RootPage() {
   // buat hook
@@ -28,8 +28,10 @@ export default function RootPage() {
           <div className="w-1/2">
             <h1 className="text-6xl font-bold mb-6">Electronic Hub</h1>
             <p className="text-gray-300 text-xl mb-8 max-w-xl">
-              Website katalog elektronik ini menyediakan berbagai pilihan produk elektronik terkini dengan informasi lengkap,
-              harga terbaik, dan fitur pencarian yang memudahkan Anda menemukan produk sesuai kebutuhan.
+              Website katalog elektronik ini menyediakan berbagai pilihan produk
+              elektronik terkini dengan informasi lengkap, harga terbaik, dan
+              fitur pencarian yang memudahkan Anda menemukan produk sesuai
+              kebutuhan.
             </p>
           </div>
           <div className="w-1/2">
@@ -57,6 +59,14 @@ export default function RootPage() {
           {/* tampilkan data barang */}
           {Object.values(getValue).map((data: any, index: number) => (
             <div key={index}>
+              <div>
+                <Image
+                  src="/images/foto.png"
+                  alt={data.nama_barang}
+                  width={500}
+                  height={300}
+                />
+              </div>
               <div>
                 {data.nama_barang} - {data.deskripsi} - {data.harga}
               </div>
