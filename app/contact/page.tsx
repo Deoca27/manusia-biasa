1e69f8766af3539b0ee3d55a048f41fd70f0d97e
rendikayp01 <rendikayogapamungkas@gmail.com>
2025-01-24 02:51:17 +0700

    rendika : Menambahkan lagi tata letak fleksibel tengah

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -50,9 +50,11 @@ export default function ContactPage() {
                 <p className="text-gray-700">Jl. Zainal Abidin Pagar Alam No. 9-11, Bandar Lampung</p>
               </div>
               <div className="flex items-center">
-              <FaPhone className="text-blue-600 mr-4 text-2xl" />
-              <p className="text-gray-700">(+62) 721 787 456</p>
-
+                <FaPhone className="text-blue-600 mr-4 text-2xl" />
+                <p className="text-gray-700">(+62) 721 787 456</p>
+              </div>
+              <div className="flex items-center">
+                
               </div>
 
             </div>
