7d634a47785b98df8d54a709795217ddd26a508f
maechel11 <jhoshemaechel01@gmail.com>
2025-01-24 19:26:34 +0700

    jhoshe: menambahkan text input dengan daisyUI

--- app/admin/item_controller/add/page.tsx
+++ app/admin/item_controller/add/page.tsx
@@ -5,7 +5,27 @@ export default function AdditemPage(){
    return(
     <>
     <div className="text-center pb-4 font-extrabold text-2xl">TAMBAH BARANG</div>
-        
+       <div>
+       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
+       
+       />
+
+       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
+       
+       />
+
+       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
+       
+       />
+
+       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
+       
+       />
+
+       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
+       
+       />
+        </div> 
     </>
    );
 }
