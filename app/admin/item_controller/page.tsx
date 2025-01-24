776684ff99a239be8249aecdf4ef8d848c5964e2
maechel11 <jhoshemaechel01@gmail.com>
2025-01-24 16:05:21 +0700

    jhoshe: panggil data deskripsi

--- app/admin/item_controller/page.tsx
+++ app/admin/item_controller/page.tsx
@@ -52,7 +52,7 @@ export default function BarangPage() {
               <td>{index + 1}</td>
               <td><Image src={data.image_url} alt="gambar" width={100} height={100} priority/></td>
               <td>{data.nama_barang}</td>
-              <td></td>
+              <td>{data.deskripsi}</td>
               <td></td>
               <td></td>
               <td></td>
