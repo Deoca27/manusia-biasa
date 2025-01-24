1cbe3e81904e2bbec9e4e9e2f662b96cb0f2c14b
maechel11 <jhoshemaechel01@gmail.com>
2025-01-24 19:30:40 +0700

    jhoshe: mengganti salah satu textinput menjadi select dengan daisyUI

--- app/admin/item_controller/add/page.tsx
+++ app/admin/item_controller/add/page.tsx
@@ -1,31 +1,33 @@
 "use client";
 
-export default function AdditemPage(){
+export default function AdditemPage() {
 
-   return(
-    <>
-    <div className="text-center pb-4 font-extrabold text-2xl">TAMBAH BARANG</div>
-       <div>
-       <input type="text" placeholder="Nama Barang" className="input input-bordered w-full max-w-xs" 
-       
-       />
+    return (
+        <>
+            <div className="text-center pb-4 font-extrabold text-2xl">TAMBAH BARANG</div>
+            <div>
+                <input type="text" placeholder="Nama Barang" className="input input-bordered w-full max-w-xs"
 
-       <input type="text" placeholder="Deskripsi" className="input input-bordered w-full max-w-xs" 
-       
-       />
+                />
 
-       <input type="text" placeholder="harga" className="input input-bordered w-full max-w-xs" 
-       
-       />
+                <input type="text" placeholder="Deskripsi" className="input input-bordered w-full max-w-xs"
 
-       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
-       
-       />
+                />
 
-       <input type="text" placeholder="link_product" className="input input-bordered w-full max-w-xs" 
-       
-       />
-        </div> 
-    </>
-   );
+                <input type="text" placeholder="harga" className="input input-bordered w-full max-w-xs"
+
+                />
+
+                <select className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>Who shot first?</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                </select>
+
+                <input type="text" placeholder="link_product" className="input input-bordered w-full max-w-xs"
+
+                />
+            </div>
+        </>
+    );
 }
