d9b28ae6b11f2cab0116cc092e704742506a92a4
maechel11 <jhoshemaechel01@gmail.com>
2025-01-24 19:20:13 +0700

    jhoshe:menambahkan tailwind text-2xl

--- app/admin/item_controller/add/page.tsx
+++ app/admin/item_controller/add/page.tsx
@@ -4,7 +4,7 @@ export default function AdditemPage(){
 
    return(
     <>
-    <div className="text-center pb-4 font-extrabold">TAMBAH BARANG</div>
+    <div className="text-center pb-4 font-extrabold text-2xl">TAMBAH BARANG</div>
         
     </>
    );
