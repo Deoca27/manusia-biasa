c62b3f34f96e931f016a7f0b0de70b3ee50ca95c
rendikayp01 <rendikayogapamungkas@gmail.com>
2025-01-25 01:00:09 +0700

    rendika : Menambahkan gaya kolom input.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -93,7 +93,7 @@ export default function ContactPage() {
                     value={formData.email}
                     onChange={handleChange}
                     required
-
+                    className="w-full border-2 border-blue-200 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
                   />
                 </div>
               </div>
