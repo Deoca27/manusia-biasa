1bbba86f129e3cdb1de38cd5b567b9f2bb8f296e
maechel11 <jhoshemaechel01@gmail.com>
2025-01-24 19:19:54 +0700

    jhoshe:menambahkan tailwind font-extrabold

--- app/admin/item_controller/add/page.tsx
+++ app/admin/item_controller/add/page.tsx
@@ -4,7 +4,7 @@ export default function AdditemPage(){
 
    return(
     <>
-    <div className="text-center pb-4">TAMBAH BARANG</div>
+    <div className="text-center pb-4 font-extrabold">TAMBAH BARANG</div>
         
     </>
    );
