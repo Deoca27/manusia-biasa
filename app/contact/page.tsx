d739805cdfcd61abe4a303a3dd6d709d8588906f
rendikayp01 <rendikayogapamungkas@gmail.com>
2025-01-25 01:34:04 +0700

    rendika : Menambahkan tombol interaktif di halaman.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -108,6 +108,7 @@ export default function ContactPage() {
                 className="w-full border-2 border-blue-200 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 ></textarea>
               </div>
+              <button></button>
             </form>
 
           </motion.div>
