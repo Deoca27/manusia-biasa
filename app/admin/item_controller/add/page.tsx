bfa219d1b4714510142facbc8161363b02f24f08
maechel11 <jhoshemaechel01@gmail.com>
2025-01-24 20:16:55 +0700

    jhoshe: menambahkan onClick pada button Simpan

--- app/admin/item_controller/add/page.tsx
+++ app/admin/item_controller/add/page.tsx
@@ -93,7 +93,7 @@ export default function AdditemPage() {
                     }}
                 />
 
-                <button className="btn col-end-7">Simpan</button>
+                <button className="btn col-end-7" onClick={setSaveData}>Simpan</button>
                 <Link href={"/admin/item_controller"} className="btn btn-neutral">Batal</Link>
             </div>
         </>
