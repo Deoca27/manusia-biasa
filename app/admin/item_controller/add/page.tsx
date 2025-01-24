0cb445aa83f7bd512e60e4aaa8ce426f3fab140d
maechel11 <jhoshemaechel01@gmail.com>
2025-01-24 19:29:37 +0700

    jhoshe: mengganti teks placeholder text input

--- app/admin/item_controller/add/page.tsx
+++ app/admin/item_controller/add/page.tsx
@@ -6,7 +6,15 @@ export default function AdditemPage(){
     <>
     <div className="text-center pb-4 font-extrabold text-2xl">TAMBAH BARANG</div>
        <div>
-       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
+       <input type="text" placeholder="Nama Barang" className="input input-bordered w-full max-w-xs" 
+       
+       />
+
+       <input type="text" placeholder="Deskripsi" className="input input-bordered w-full max-w-xs" 
+       
+       />
+
+       <input type="text" placeholder="harga" className="input input-bordered w-full max-w-xs" 
        
        />
 
@@ -14,15 +22,7 @@ export default function AdditemPage(){
        
        />
 
-       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
-       
-       />
-
-       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
-       
-       />
-
-       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" 
+       <input type="text" placeholder="link_product" className="input input-bordered w-full max-w-xs" 
        
        />
         </div> 
