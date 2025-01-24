ac9dc26e4dce76e712b40024a1798c3c3458b1d1
rendikayp01 <rendikayogapamungkas@gmail.com>
2025-01-25 01:36:16 +0700

    rendika : Menambahkan fungsi untuk mengirimkan formulir.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -108,7 +108,11 @@ export default function ContactPage() {
                 className="w-full border-2 border-blue-200 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 ></textarea>
               </div>
-              <button></button>
+              <button
+              type="submit" 
+              >
+
+              </button>
             </form>
 
           </motion.div>
