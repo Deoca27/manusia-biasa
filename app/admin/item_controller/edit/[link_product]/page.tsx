88011257272809ad1ce6e406434ab2cf21cec726
maechel11 <jhoshemaechel01@gmail.com>
2025-01-25 16:06:37 +0700

    jhoshe: merapihkan text input dengan col

--- app/admin/item_controller/edit/[link_product]/page.tsx
+++ app/admin/item_controller/edit/[link_product]/page.tsx
@@ -11,15 +11,15 @@ export default function EditPage({ params }: { params: { link_product: string } 
             <div className="text-center font-extrabold text-2xl">EDIT DATA BARANG</div>
             
                 <div className="grid grid-cols-10 gap-4 mt-16">
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs " />
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                    <select className="select select-bordered w-full max-w-xs">
+                    <input type="text" placeholder="Type here" className="input input-bordered col-start-4 col-span-4" />
+                    <input type="text" placeholder="Type here" className="input input-bordered col-start-4 col-span-4" />
+                    <input type="text" placeholder="Type here" className="input input-bordered col-start-4 col-span-4" />
+                    <select className="select select-bordered col-start-4 col-span-4">
                         <option disabled selected>Who shot first?</option>
                         <option>Han Solo</option>
                         <option>Greedo</option>
                     </select>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Type here" className="input input-bordered col-start-4 col-span-4" />
 
 
                 </div>
