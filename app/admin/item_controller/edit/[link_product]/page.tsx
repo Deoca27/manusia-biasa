294732a7ec4297e83051a6fa5daee5957e43be43
maechel11 <jhoshemaechel01@gmail.com>
2025-01-25 16:14:09 +0700

    jhoshe: mengubah placeholder dan menambahkan value pada kategori

--- app/admin/item_controller/edit/[link_product]/page.tsx
+++ app/admin/item_controller/edit/[link_product]/page.tsx
@@ -9,21 +9,27 @@ export default function EditPage({ params }: { params: { link_product: string } 
     return (
         <>
             <div className="text-center font-extrabold text-2xl">EDIT DATA BARANG</div>
-            
-                <div className="grid grid-cols-10 gap-4 mt-16">
-                    <input type="text" placeholder="Type here" className="input input-bordered col-start-4 col-span-4" />
-                    <input type="text" placeholder="Type here" className="input input-bordered col-start-4 col-span-4" />
-                    <input type="text" placeholder="Type here" className="input input-bordered col-start-4 col-span-4" />
-                    <select className="select select-bordered col-start-4 col-span-4">
-                        <option disabled selected>Who shot first?</option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
-                    </select>
-                    <input type="text" placeholder="Type here" className="input input-bordered col-start-4 col-span-4" />
+
+            <div className="grid grid-cols-10 gap-4 mt-16">
+                <input type="text" placeholder="Nama Barang" className="input input-bordered col-start-4 col-span-4" />
+                <input type="text" placeholder="Deskripsi" className="input input-bordered col-start-4 col-span-4" />
+                <input type="text" placeholder="Harga" className="input input-bordered col-start-4 col-span-4" />
+                <select defaultValue={""} className="select select-bordered col-start-4 col-span-4">
+                    <option value={""} disabled>
+                        Pilih kategori Elektronik
+                    </option>
+                    <option value={"Handphone"}>Handphone</option>
+                    <option value={"Komputer"}>Komputer</option>
+                    <option value={"Laptop"}>Laptop</option>
+                    <option value={"TV"}>TV</option>
+                    <option value={"Accesoris"}>Accesoris</option>
+                    <option value={"Console"}>Console</option>
+                </select>
+                <input type="text" placeholder="link product" className="input input-bordered col-start-4 col-span-4" />
 
 
-                </div>
-            
+            </div>
+
         </>
     );
 
