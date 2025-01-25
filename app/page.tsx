d94da53a8c8c8c6e18007a1417fc05b476ed946b
Deoca27 <pikirsendiri953@gmail.com>
2025-01-25 09:21:36 +0700

    deo : mengubah warna bacground gambar

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function RootPage() {
           {/* tampilkan data barang */}
           {Object.values(getValue).map((data: any, index: number) => (
             <div key={index}>
-              <div className="w-full aspect-square">
+              <div className="w-full aspect-square bg-gray-100">
                 <Image
                   src={data.image_url}
                   alt={data.nama_barang}
