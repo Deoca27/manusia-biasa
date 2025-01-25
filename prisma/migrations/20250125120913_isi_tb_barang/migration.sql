366d997deb390b55e3cf3f543c0c69132e9a580d
Deoca27 <pikirsendiri953@gmail.com>
2025-01-25 19:46:17 +0700

    deo : tambah migration untuk isi tb_barang

---
+++ prisma/migrations/20250125120913_isi_tb_barang/migration.sql
@@ -0,0 +1,6 @@
+-- InsertInto
+INSERT INTO tb_barang (nama_barang, deskripsi, harga, kategori, image_url, link_product) VALUES 
+('Samsung Galaxy A05s', '6/128GB - Black', 1799000, 'Handphone', '/images/hp1.jpeg','https://id.shp.ee/pE85T75'),
+('Lenovo LOQ Gaming', 'Intel Core i5 Win11 12GB 512GB SSD NVIDIA RTX 3050 6GB', 12624550, 'Laptop', '/images/laptop1.jpeg','https://id.shp.ee/J6PoZJZ'),
+('Xiaomi Mi Google TV', 'Xiaomi Mi Google TV Pro Series 32/43/55', 2050000, 'TV', '/images/tv1.jpeg','https://tokopedia.link/btQJ6OizrQb'),
+('Playstation 5', 'Sony PS5 Slim Console Disc Edition', 8909000, 'Console', '/images/consol1.jpeg','https://tokopedia.link/IpeIuJnzrQb');
