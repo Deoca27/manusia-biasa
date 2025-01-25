5df86a8c4fbfc1afc503f2974841c02bde9fc13f
Deoca27 <pikirsendiri953@gmail.com>
2025-01-25 09:24:37 +0700

    deo : menngubah warna border

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function RootPage() {
           {/* tampilkan data barang */}
           {Object.values(getValue).map((data: any, index: number) => (
             <div key={index}>
-              <div className="w-full aspect-square bg-gray-100 border-b">
+              <div className="w-full aspect-square bg-gray-100 border-b border-gray-200">
                 <Image
                   src={data.image_url}
                   alt={data.nama_barang}
